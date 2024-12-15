55217431a63d48b976cc840cd935d4889b2bec71
Sonicetorock <sonishgames@gmail.com>
2024-12-15 23:32:41 +0530

    Completed backend setup and tested all endpoints

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ app.use(express.json());
 // Import routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const authorRoutes = require('./routes/authorRoutes');
+const readerRoutes = require('./routes/readerRoutes');
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/authors', authorRoutes);
+app.use('/api/v1/readers',  readerRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
